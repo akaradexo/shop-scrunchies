8aefdf8c898fe2a061a4403672e10c581997e967
xR0v3Rx <infoashu.k@gmail.com>
2021-07-26 12:02:55 +0530

    wa

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,3 +1,3 @@
 import Commerce from '@chec/commerce.js';
 
-export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY, true);
+export const commerce = new Commerce(process.env.pk_test_3072172fa72bcd29cbf7d098a217393f1bc5e0561d7aa, true);
